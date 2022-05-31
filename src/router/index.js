30ee753c6e5451c12e3f28c2b52f58be809bb4ea
Théo Rosset <theo.rosset@outlook.fr>
2022-05-31 11:02:48 +0200

    recuperation des articles et des utilisateur avec vuex

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import allArticle from "../views/allArticle.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+  },
+  {
+    path: "/allArticle",
+    name: "allArticle",
+    component: allArticle,
   },
 ];
 
