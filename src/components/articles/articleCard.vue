5f826f9aec29319f82c2918d9ffca28853aab482
Théo Rosset <theo.rosset@outlook.fr>
2022-05-31 13:33:21 +0200

    composant pour la liste(ul) des articles,composant (li) + début de la mise en place

---
+++ src/components/articles/articleCard.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="articleCard">
+    <h1>Articles de notre blog</h1>
+    <li v-for="article of articles" :key="article.id" class="articleElement">
+      <img src="#" alt="#" />
+      <h2></h2>
+    </li>
+  </div>
+</template>
+
+<script>
+/*eslint-disable*/
+export default {
+  name: "articleCard",
+  props: { articles: { type: Array, default: () => [] },
+           users: { type: Array, default: () => [] }  },
+  methods:{    
+      }
+  }
+;
+</script>
+<style scoped>
+h1{
+    font-size: 21px;
+    font-weight: bold;
+    max-width: 500px;
+    margin: auto;
+    margin-top: 20px;
+}
+
+.articleElement{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    max-width: 500px;
+    margin: auto;
+    margin-top: 30px;
+    border: 1px solid black;
+    border-radius: 5px;
+}
+
+
+</style>
---
+++ src/components/articles/articleList.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul>
+    <articleCard :articles="articles" :users="users" />
+  </ul>
+</template>
+<script>
+import articleCard from "./articleCard.vue";
+export default {
+  name: "articleList",
+  props: {
+    articles: { type: Array, default: () => [] },
+    users: { type: Array, default: () => [] },
+  },
+  components: { articleCard },
+};
+</script>
