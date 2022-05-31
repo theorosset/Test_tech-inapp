d6f396d8bed5af24038a7937ba3d58dcbcf7a72f
Théo Rosset <theo.rosset@outlook.fr>
2022-05-31 19:02:56 +0200

    revue du composant

--- src/components/articles/articleCard.vue
+++ src/components/articles/articleCard.vue
@@ -1,16 +1,11 @@
 <template>
   <div class="articleCard">
-    <h1>Articles de notre blog</h1>
-    <router-link
-      v-for="article of articles"
-      :key="article.id"
-      :to="{ name: 'articleRead', params: { id: article.id } }"
-    >
-      <li class="articleElement">
+    <li v-for="article of articles" :key="article.id" class="articleElement">
+      <router-link :to="{ name: 'articleRead', params: { id: article.id } }">
         <h2>{{ article.title }}</h2>
         <p>{{ article.body }}</p>
-      </li>
-    </router-link>
+      </router-link>
+    </li>
   </div>
 </template>
 
@@ -33,13 +28,6 @@ a{
     text-decoration: none;
 }
 
-h1{
-    font-size: 21px;
-    font-weight: bold;
-    max-width: 500px;
-    margin: auto;
-    margin-top: 20px;
-}
 h2{
     @include textEllipsis;
     @include marginLeftRight(10px,10px);
@@ -57,8 +45,7 @@ p{
 }
 
 .articleElement{
-    display: flex;
-    flex-direction: column;
+   @include flexColumn;
     max-width: 500px;
     margin: auto;
     margin-top: 30px;
