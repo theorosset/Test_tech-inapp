a44e2906f08538a25e4503eab3ad36471cce9f04
Théo Rosset <theo.rosset@outlook.fr>
2022-05-31 11:05:53 +0200

    récuperation des article et des utilisateur avec vuex

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,36 @@
 import { createStore } from "vuex";
+import axios from "axios";
 
+//recuperation des utilisateur
+function getUsers() {
+  return axios.get("https://jsonplaceholder.typicode.com/users");
+}
+function getArticles() {
+  return axios.get("https://jsonplaceholder.typicode.com/posts");
+}
 export default createStore({
-  state: {},
+  state: {
+    users: [],
+    articles: [],
+  },
   getters: {},
-  mutations: {},
-  actions: {},
+  mutations: {
+    SET_USERS(state, users) {
+      state.users = users;
+    },
+    SET_ARTICLES(state, articles) {
+      state.articles = articles;
+    },
+  },
+  actions: {
+    async axiosUsers({ commit }) {
+      const users = await getUsers();
+      commit("SET_USERS", users.data);
+    },
+    async axiosArticles({ commit }) {
+      const articles = await getArticles();
+      commit("SET_ARTICLES", articles.data);
+    },
+  },
   modules: {},
 });
