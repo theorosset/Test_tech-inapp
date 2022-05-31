07b5bc9ae64baee041567247703f0fb7605c2232
Théo Rosset <theo.rosset@outlook.fr>
2022-05-31 15:09:23 +0200

    ajout des articles du blog + au click renvoie vers l'article cliquer

--- src/components/articles/articleCard.vue
+++ src/components/articles/articleCard.vue
@@ -1,25 +1,37 @@
 <template>
   <div class="articleCard">
     <h1>Articles de notre blog</h1>
-    <li v-for="article of articles" :key="article.id" class="articleElement">
-      <img src="#" alt="#" />
-      <h2></h2>
-    </li>
+    <router-link
+      v-for="article of articles"
+      :key="article.id"
+      :to="{ name: 'articleRead', params: { id: article.id } }"
+    >
+      <li class="articleElement">
+        <h2>{{ article.title }}</h2>
+        <p>{{ article.body }}</p>
+      </li>
+    </router-link>
   </div>
 </template>
 
 <script>
 /*eslint-disable*/
+
 export default {
   name: "articleCard",
   props: { articles: { type: Array, default: () => [] },
            users: { type: Array, default: () => [] }  },
-  methods:{    
-      }
-  }
-;
+  methods:{}
+  };
 </script>
-<style scoped>
+
+
+<style lang="scss" scoped>
+@import "@/assets/mixin.scss";
+a{
+    color: black;
+    text-decoration: none;
+}
 h1{
     font-size: 21px;
     font-weight: bold;
@@ -27,11 +39,25 @@ h1{
     margin: auto;
     margin-top: 20px;
 }
+h2{
+    @include textEllipsis;
+    @include marginLeftRight(10px,10px);
+    font-size: 19px;
+    font-weight: bold;
+    margin-top: 10px
+}
+
+p{
+    @include textEllipsis;
+    @include marginLeftRight(10px,10px);
+    margin-top: 15px;
+    margin-bottom: 10px;
+    
+}
 
 .articleElement{
     display: flex;
     flex-direction: column;
-    align-items: center;
     max-width: 500px;
     margin: auto;
     margin-top: 30px;
