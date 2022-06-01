c38c76d631d0c633cabe5e1b9b742b6dfeb71e50
Théo Rosset <theo.rosset@outlook.fr>
2022-06-01 18:23:46 +0200

    ajout public path

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,10 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
 });
+
+const publicPath =
+  process.env.NODE_ENV === "production" ? "/Test_tech-inapp/" : "/";
+
+module.exports = {
+  publicPath: publicPath,
+};
